89c60de1b3f33898f317b06051493fa6482cfe78
Daniel Hu <danielhu83@outlook.com>
2019-03-10 16:44:43 +0800

    Add IPV6 code

--- HelloSocket/main.cc
+++ HelloSocket/main.cc
@@ -8,11 +8,12 @@
 #include <string.h>
 
 #define MAX_LINE 1024
-
+#define IPV4
 /*
 test sample: This IP and port works well
 GetTime 128.138.141.172 13
 */
+#ifdef IPV4
 int main(int argc, char **argv)
 {
 	if (argc != 3)
@@ -63,3 +64,58 @@ int main(int argc, char **argv)
 	//fprintf(stdout, "End receive data:[%s]\n", receiveline);
 	return 0;
 }
+
+#else
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		fprintf(stdout, "usage: <IPADRESS> <port>!\n");
+		return -1;
+	}
+	int sockfd, n;
+	char receiveline[MAX_LINE + 1];
+	struct sockaddr_in6 serveraddr;
+
+	if ((sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0)
+	{
+		fprintf(stdout, "Socket error!\n");
+		return 1;
+	}
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin6_family = AF_INET;
+	
+	int portnumber = atoi(argv[2]);
+	//printf("testNumber =[%d]\n", portnumber);
+
+	serveraddr.sin6_port = htons(portnumber);
+	if (inet_pton(AF_INET6, argv[1], &serveraddr.sin6_addr) <= 0)
+	{
+		fprintf(stdout, "inet_pton error!\n");
+		return 1;
+	}
+	if (connect(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+	{
+		fprintf(stdout, "connect error!\n");
+		return 1;
+	}
+	while((n = read(sockfd, receiveline, MAX_LINE)) > 0)
+	{
+		receiveline[n] = '\0';
+		if (fputs(receiveline, stdout) == EOF)
+		{
+			fprintf(stdout, "fputs error!\n");
+			return 1;
+		}
+		fprintf(stdout, "receive data:[%s]\n", receiveline);
+	}
+	if (n < 0)
+	{
+		fprintf(stdout, "read error!\n");
+		return 1;
+	}
+	//fprintf(stdout, "End receive data:[%s]\n", receiveline);
+	return 0;
+
+}
+#endif
